5286433e609f48de5157a07102a907ade9748161
Joey Cardosi <jcatfour@gmail.com>
2023-04-06 02:43:00 -0400

    add wizard

--- source/main.lua
+++ source/main.lua
@@ -5,12 +5,13 @@ import "CoreLibs/timer"
 
 import "knight"
 import "giant"
+import "wizard"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
 local function initialize()
-	local playerInstance = Giant(200, 200)
+	local playerInstance = Wizard(200, 200)
 	playerInstance:add()
 end
 
