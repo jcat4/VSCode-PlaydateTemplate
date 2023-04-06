c665cf9af5422d78c3e2c7342af70bee8dc4fcf4
Joey Cardosi <jcatfour@gmail.com>
2023-04-06 02:31:13 -0400

    add a knight and projectiles

--- source/main.lua
+++ source/main.lua
@@ -1,28 +1,21 @@
-import "dvd" -- DEMO
-local dvd = dvd(1, -1) -- DEMO
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
-local gfx <const> = playdate.graphics
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
+import "knight"
 
-local function loadGame()
-	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
-	math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
-	gfx.setFont(font) -- DEMO
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+local function initialize()
+	local playerInstance = Knight(200, 200)
+	playerInstance:add()
 end
 
-local function updateGame()
-	dvd:update() -- DEMO
-end
+initialize()
 
-local function drawGame()
-	gfx.clear() -- Clears the screen
-	dvd:draw() -- DEMO
-end
-
-loadGame()
-
-function playdate.update()
-	updateGame()
-	drawGame()
-	playdate.drawFPS(0,0) -- FPS widget
+function pd.update()
+	gfx.sprite.update()
+	pd.timer.updateTimers()
 end
